fa60dc4b6e9f0bb395b2fd9bceedc156e311365b
Th3Tw0 <thetwo@gmx.net>
2015-04-11 05:10:02 +0200

    fixed some issues
    added comments

--- A_in_B.java
+++ A_in_B.java
@@ -1,21 +1,21 @@
 /**
- * Created by argannor on 10.04.15.
+ * Created by Soeren Berken-Mersmann on 10.04.15.
+ * Alpha Composition using the A in B operator
  */
 public class A_in_B extends Alpha_Blending {
 
     @Override
     protected int calculateColor(int color1, int color2, float alpha1Normalized, float alpha2Normalized, float alphaNorm) {
-        if(alpha2Normalized == 0) {
-            return 0;
-        }
-        return color2;
+        // the only visible color is from A
+        return color1;
     }
 
     @Override
     protected float calculateAlpha(float alpha1Normalized, float alpha2Normalized) {
-        if(alpha2Normalized == 0) {
-            return 0;
+        if(alpha2Normalized > 0 && alpha1Normalized > 0) {
+            // if both alpha channels are greater then zero, we're in the intersecting set.
+            return super.calculateAlpha(alpha1Normalized, alpha2Normalized);
         }
-        return super.calculateAlpha(alpha1Normalized, alpha2Normalized);
+        return 0;
     }
 }
--- A_out_B.java
+++ A_out_B.java
@@ -1,20 +1,22 @@
 /**
- * Created by argannor on 10.04.15.
+ * Created by Soeren Berken-Mersmann on 10.04.15.
+ * Alpha Composition using the A held out by B operator
+ * (short A out B)
  */
 public class A_out_B extends Alpha_Blending {
     @Override
     protected int calculateColor(int color1, int color2, float alpha1Normalized, float alpha2Normalized, float alphaNorm) {
-        if(alpha1Normalized != 0 && alpha2Normalized < 1) {
-            return color1;
-        }
-        return 0;
+        // The color is always Color_A
+        return color1;
     }
 
     @Override
     protected float calculateAlpha(float alpha1Normalized, float alpha2Normalized) {
-        if (alpha2Normalized == 0)
+        // If alpha_B == 0, we're outside of B, therefore we can return alpha_A
+        if (alpha2Normalized == 0) {
             return alpha1Normalized;
-        // TODO
-        return super.calculateAlpha(alpha1Normalized, alpha2Normalized);
+        }
+        // otherwise we calculate the alpha value by multiplying alpha_A with alpha_B
+        return alpha1Normalized * alpha2Normalized;
     }
 }
